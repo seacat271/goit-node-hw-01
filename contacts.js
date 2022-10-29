2b86fd52f5c0f56daabb12cef5603a19455cede4
seacat271 <seacat271083@gmail.com>
2022-10-29 19:56:08 +0300

    common-js

---
+++ contacts.js
@@ -0,0 +1,26 @@
+const fs =  require("fs");
+const path = require("path");
+const contactsPath = path.dirname("contacts.js");
+console.log(contactsPath)
+
+/*
+ * Розкоментуйте і запиши значення
+ * const contactsPath = ;
+ */
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+    // ...твій код
+  }
+  
+  function getContactById(contactId) {
+    // ...твій код
+  }
+  
+  function removeContact(contactId) {
+    // ...твій код
+  }
+  
+  function addContact(name, email, phone) {
+    // ...твій код
+  }
