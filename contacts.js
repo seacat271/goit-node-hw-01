3eac22f5f80d57780cb22e6e975cd0b89911816d
seacat271 <seacat271083@gmail.com>
2022-10-29 22:00:33 +0300

    add function

--- contacts.js
+++ contacts.js
@@ -1,26 +1,55 @@
-const fs =  require("fs");
+const fs =  require("fs").promises;
 const path = require("path");
-const contactsPath = path.dirname("contacts.js");
-console.log(contactsPath)
 
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
+const contactsPath = path.resolve("./db/contacts.json");
+const testPath = path.resolve("./test/test.txt");
+// console.log(contactsPath)
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-    // ...твій код
+
+const data = fs.readFile(contactsPath, "utf-8");
+        // await fs.writeFile(testPath, data, "utf-8")
+
+
+
+async function listContacts  ()  {
+    try {
+        return await fs.readFile(contactsPath, "utf-8");
+    } catch (error) {
+        console.log(error)
+  }
+
+}
+  
+async  function getContactById(contactId) {
+   try {
+    return await fs.readFile(contactsPath, "utf-8").filter(item => item.id === contactId)
+    } catch (error) {
+        console.log(error)
+    }
+}
+  
+async  function removeContact(contactId) {
+    try {
+     const newData = await fs.readFile(contactsPath, "utf-8").filter(item => item.id !== contactId);
+     await fs.writeFile(contactsPath, newData, "utf-8")
+     return contactId
+    } catch (error) {
+        console.log(error)
+    }
   }
   
-  function getContactById(contactId) {
-    // ...твій код
-  }
-  
-  function removeContact(contactId) {
-    // ...твій код
-  }
-  
-  function addContact(name, email, phone) {
-    // ...твій код
-  }
+async function addContact(name, email, phone) {
+try {
+    const id = data[data.length - 1].id + 1
+return [...data, {id, name, email, phone }]
+} catch (error) {
+    console.log(error)
+}
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+}
