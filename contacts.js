7cf134e6d3b597f5ffa8413cd5b4722bca821b6c
seacat271 <seacat271083@gmail.com>
2022-10-30 14:03:18 +0300

    step#3

--- contacts.js
+++ contacts.js
@@ -1,27 +1,25 @@
 const fs =  require("fs").promises;
 const path = require("path");
-
 const contactsPath = path.resolve("./db/contacts.json");
-const testPath = path.resolve("./test/test.txt");
-// console.log(contactsPath)
-const data = async path => {
+
+const dataGet = async path => {
     return JSON.parse(await fs.readFile(path, "utf-8"))
 };
-// await fs.writeFile(testPath, data, "utf-8")
-
+const dataChange = async (path, newData) => {
+    return await fs.writeFile(path, JSON.stringify(newData), "utf-8")
+}
 
 async function listContacts()  {
     try {
-         return await data(contactsPath);
+         return await dataGet(contactsPath);
     } catch (error) {
         console.log(error)
   }
-
 }
-// contacts.filter(item => item.id === contactId)
+
 async function getContactById(contactId) {
    try {
-    const contacts = await data(contactsPath)
+    const contacts = await dataGet(contactsPath)
    return contacts.filter(item => item.id === contactId)
     } catch (error) {
         console.log(error)
@@ -30,10 +28,9 @@ async function getContactById(contactId) {
   
 async function removeContact(contactId) {
     try {
-        const contacts = await data(contactsPath);
+        const contacts = await dataGet(contactsPath);
         const newData = contacts.filter(item => item.id !== contactId);
-
-        await fs.writeFile(contactsPath, JSON.stringify(newData), "utf-8")
+        await dataChange(contactsPath, newData)
         return contactId
     } catch (error) {
         console.log(error)
@@ -42,13 +39,14 @@ async function removeContact(contactId) {
   
 async function addContact(name, email, phone) {
 try {
-    const contacts = await data(contactsPath);
-    const id = contacts[contacts.length - 1].id + 1
+    const contacts = await dataGet(contactsPath);
+    const id = +contacts[contacts.length - 1].id + 1 + "";
     const newData = [...contacts, {id, name, email, phone }]
-    await fs.writeFile(contactsPath, newData, "utf-8")
-} catch (error) {
-    console.log(error)
-}
+    await dataChange(contactsPath, newData)
+    return id;
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const {   listContacts, getContactById, removeContact, addContact} = require("./contacts");
 
-// addContact("Kirill", "seacat@mail.com", "04040004040");
-// console.log(addContact("Kirill", "seacat@mail.com", "04040004040"))
+
+
 // listContacts().then(data => console.log(data))
-getContactById("4").then(data => console.log(data))
-// removeContact("3").then(data => console.log(data))
+// getContactById("4").then(data => console.log(data))
+// removeContact("5").then(data => console.log(data))
+// addContact("Kirill", "seacat@mail.com", "04040004040").then(data => console.log(data))
