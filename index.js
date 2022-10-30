73a3f24386924822d8c5998510d86ca2047168f9
seacat271 <seacat271083@gmail.com>
2022-10-30 14:13:33 +0300

    add switch

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const argv = require("yargs").argv;
 const {   listContacts, getContactById, removeContact, addContact} = require("./contacts");
 
-console.log(argv)
+// console.log(argv)
 
 // listContacts().then(data => console.log(data))
 // getContactById("4").then(data => console.log(data))
 // removeContact("5").then(data => console.log(data))
 // addContact("Kirill", "seacat@mail.com", "04040004040").then(data => console.log(data))
+
+function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case "list":
+        listContacts()
+        break;
+  
+      case "get":
+        getContactById(id)
+        break;
+  
+      case "add":
+        addContact(name, email, phone)
+        break;
+  
+      case "remove":
+        removeContact(id)
+        break;
+  
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  }
+
+  invokeAction(argv);
