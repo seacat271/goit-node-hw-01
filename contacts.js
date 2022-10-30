73e130d42b1e558a82814f975af9c8850ddc7782
seacat271 <seacat271083@gmail.com>
2022-10-30 11:12:53 +0300

    step#2

--- contacts.js
+++ contacts.js
@@ -4,16 +4,14 @@ const path = require("path");
 const contactsPath = path.resolve("./db/contacts.json");
 const testPath = path.resolve("./test/test.txt");
 // console.log(contactsPath)
-
-
-const data = fs.readFile(contactsPath, "utf-8");
-        // await fs.writeFile(testPath, data, "utf-8")
+const data = (path) => fs.readFile(path, "utf-8");
+// await fs.writeFile(testPath, data, "utf-8")
 
 
 
 async function listContacts  ()  {
     try {
-        return await fs.readFile(contactsPath, "utf-8");
+        return await data(contactsPath);
     } catch (error) {
         console.log(error)
   }
@@ -22,7 +20,7 @@ async function listContacts  ()  {
   
 async  function getContactById(contactId) {
    try {
-    return await fs.readFile(contactsPath, "utf-8").filter(item => item.id === contactId)
+    return await data(contactsPath).filter(item => item.id === contactId)
     } catch (error) {
         console.log(error)
     }
@@ -30,7 +28,7 @@ async  function getContactById(contactId) {
   
 async  function removeContact(contactId) {
     try {
-     const newData = await fs.readFile(contactsPath, "utf-8").filter(item => item.id !== contactId);
+     const newData = await data(contactsPath).filter(item => item.id !== contactId);
      await fs.writeFile(contactsPath, newData, "utf-8")
      return contactId
     } catch (error) {
@@ -40,8 +38,10 @@ async  function removeContact(contactId) {
   
 async function addContact(name, email, phone) {
 try {
-    const id = data[data.length - 1].id + 1
-return [...data, {id, name, email, phone }]
+    const contacts = await data(contactsPath);
+    const id = contacts[contacts.length - 1].id + 1
+    const newData = [...contacts, {id, name, email, phone }]
+    await fs.writeFile(contactsPath, newData, "utf-8")
 } catch (error) {
     console.log(error)
 }
--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log("Hello world")
+const {   listContacts, getContactById, removeContact, addContact} = require("./contacts");
+
+addContact("Kirill", "seacat@mail.com", "04040004040");
+console.log(addContact("Kirill", "seacat@mail.com", "04040004040"))
